697285ebf1a54972743701647cd255a2d0e64c80
Oleksandr Savenets <savenvoin@gmail.com>
2023-09-05 16:50:55 +0300

    add Dto, controller and mapper

---
+++ src/main/java/com/app/onlinebookstore/controller/BookController.java
@@ -0,0 +1,30 @@
+package com.app.onlinebookstore.controller;
+
+import com.app.onlinebookstore.dto.BookDto;
+import com.app.onlinebookstore.dto.CreateBookRequestDto;
+import com.app.onlinebookstore.service.BookService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    @GetMapping
+    public List<BookDto> findAll() {
+        return bookService.findAll();
+    }
+
+    @PostMapping
+    public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
+        return bookService.save(bookDto);
+    }
+}
---
+++ src/main/java/com/app/onlinebookstore/mapper/BookMapper.java
@@ -0,0 +1,16 @@
+package com.app.onlinebookstore.mapper;
+
+import com.app.onlinebookstore.config.MapperConfig;
+import com.app.onlinebookstore.dto.BookDto;
+import com.app.onlinebookstore.dto.CreateBookRequestDto;
+import com.app.onlinebookstore.model.Book;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(config = MapperConfig.class)
+public interface BookMapper {
+    BookDto toDto(Book book);
+
+    @Mapping(target = "id", ignore = true)
+    Book toModel(CreateBookRequestDto requestDto);
+}
