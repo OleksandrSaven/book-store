61b15980971ab30284bfd23c9cdf25edae8455cd
Oleksandr Savenets <savenvoin@gmail.com>
2023-09-10 19:34:25 +0300

    add @NotBlank annotation

--- src/main/java/com/app/onlinebookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/app/onlinebookstore/dto/CreateBookRequestDto.java
@@ -1,6 +1,7 @@
 package com.app.onlinebookstore.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
@@ -9,15 +10,19 @@ import org.hibernate.validator.constraints.Length;
 @Data
 public class CreateBookRequestDto {
     @NotNull
+    @NotBlank
     @Length(min = 1, max = 255)
     private String title;
     @NotNull
+    @NotBlank
     @Length(min = 8, max = 255)
     private String author;
     @NotNull
+    @NotBlank
     @Length(min = 10, max = 255)
     private String isbn;
     @NotNull
+    @NotBlank
     @Min(0)
     private BigDecimal price;
     private String description;
