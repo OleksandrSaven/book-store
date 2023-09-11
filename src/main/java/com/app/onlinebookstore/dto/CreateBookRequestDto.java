75c3920274f1785f5e829bec6255b95b853cb53b
Oleksandr Savenets <savenvoin@gmail.com>
2023-09-11 12:58:01 +0300

    changed @Length to @Size, add @ISBN, deleted redundent annotation

--- src/main/java/com/app/onlinebookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/app/onlinebookstore/dto/CreateBookRequestDto.java
@@ -1,28 +1,24 @@
 package com.app.onlinebookstore.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
+
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
     @NotBlank
-    @Length(min = 1, max = 255)
+    @Size(min = 1, max = 255)
     private String title;
-    @NotNull
     @NotBlank
-    @Length(min = 8, max = 255)
+    @Size(min = 8, max = 255)
     private String author;
-    @NotNull
     @NotBlank
-    @Length(min = 10, max = 255)
+    @ISBN
     private String isbn;
     @NotNull
-    @NotBlank
     @Min(0)
     private BigDecimal price;
     private String description;
