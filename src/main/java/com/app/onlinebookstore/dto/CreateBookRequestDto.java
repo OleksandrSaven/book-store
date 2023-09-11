2db440504360844dca4ae3849f3ae0955c407633
Oleksandr Savenets <savenvoin@gmail.com>
2023-09-11 13:04:37 +0300

    fix checkstyle mistakes

--- src/main/java/com/app/onlinebookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/app/onlinebookstore/dto/CreateBookRequestDto.java
@@ -1,11 +1,12 @@
 package com.app.onlinebookstore.dto;
 
-import jakarta.validation.constraints.*;
-
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 import org.hibernate.validator.constraints.ISBN;
-import org.hibernate.validator.constraints.Length;
 
 @Data
 public class CreateBookRequestDto {
