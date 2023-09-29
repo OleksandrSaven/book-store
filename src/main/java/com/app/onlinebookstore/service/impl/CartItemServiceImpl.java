99dcbafb99f30aa5299469b398bfc6c2b433cb94
Oleksandr Savenets <134528120+OleksandrSaven@users.noreply.github.com>
2023-09-29 17:49:10 +0300

    Update CartItemServiceImpl.java

--- src/main/java/com/app/onlinebookstore/service/impl/CartItemServiceImpl.java
+++ src/main/java/com/app/onlinebookstore/service/impl/CartItemServiceImpl.java
@@ -14,7 +14,6 @@ import com.app.onlinebookstore.service.CartItemService;
 import com.app.onlinebookstore.service.UserService;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
