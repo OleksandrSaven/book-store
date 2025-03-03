1772128a3fc48fe756f4cff403afd28f46ccdf8c
Oleksandr Savenets <savenvoin@gmail.com>
2025-03-03 10:03:17 +0200

    create gitHub user if last one does not exist

--- src/main/java/com/app/onlinebookstore/config/SecurityConfig.java
+++ src/main/java/com/app/onlinebookstore/config/SecurityConfig.java
@@ -14,8 +14,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -29,11 +27,6 @@ public class SecurityConfig {
     private final UserDetailsService userDetailsService;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final Oauth2AuthenticationHandler oauth2AuthenticationHandler;
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
--- src/main/java/com/app/onlinebookstore/security/Oauth2AuthenticationHandler.java
+++ src/main/java/com/app/onlinebookstore/security/Oauth2AuthenticationHandler.java
@@ -1,23 +1,41 @@
 package com.app.onlinebookstore.security;
 
+import com.app.onlinebookstore.exaption.EntityNotFoundException;
+import com.app.onlinebookstore.model.Role;
+import com.app.onlinebookstore.model.RoleName;
+import com.app.onlinebookstore.model.ShoppingCart;
+import com.app.onlinebookstore.model.User;
+import com.app.onlinebookstore.repository.RoleRepository;
+import com.app.onlinebookstore.repository.ShoppingCartRepository;
+import com.app.onlinebookstore.repository.UserRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 public class Oauth2AuthenticationHandler extends SimpleUrlAuthenticationSuccessHandler {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
+    private final ShoppingCartRepository shoppingCartRepository;
     private static final String URI_TOKEN = "http://localhost:4200/login?token=";
     private static final String EMAIL = "email";
     private final JwtUtil jwtUtil;
 
     @Override
+    @Transactional
     public void onAuthenticationSuccess(
             HttpServletRequest request,
             HttpServletResponse response,
@@ -25,6 +43,26 @@ public class Oauth2AuthenticationHandler extends SimpleUrlAuthenticationSuccessH
 
         OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
         String email = oauth2User.getAttribute(EMAIL);
+        Optional<User> user = userRepository.findByEmail(email);
+        if (user.isEmpty()) {
+            User gitHubUser = new User();
+            gitHubUser.setEmail(email);
+            gitHubUser.setFirstName(oauth2User.getAttribute("login"));
+            gitHubUser.setLastName(oauth2User.getAttribute("name"));
+            gitHubUser.setPassword(passwordEncoder.encode("password"));
+
+            Set<Role> roles= new HashSet<>();
+            Role defoultRole = roleRepository.findByRoleName(RoleName.ROLE_USER).orElseThrow(
+                    () -> new EntityNotFoundException("Current role does not exist."));
+            roles.add(defoultRole);
+            gitHubUser.setRoles(roles);
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.setUser(gitHubUser);
+            shoppingCartRepository.save(shoppingCart);
+
+            userRepository.save(gitHubUser);
+        }
         String token = jwtUtil.generateToken(email);
         response.sendRedirect(URI_TOKEN + token);
     }
